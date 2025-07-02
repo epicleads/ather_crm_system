4e6391397c39176fa15f0ab2d2c0f6f87c4d0d78
Sanjay Kankamwar <sanjaykankamwar6008@gmail.com>
2025-07-02 13:02:38 +0530

    Added Monkeypatch for Real-Time features

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import eventlet
+eventlet.monkey_patch()
 from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 from supabase.client import create_client, Client
 import csv
